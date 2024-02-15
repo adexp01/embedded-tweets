57cb023b1e5b694c79c69837eda62b1f91badfd4
Andriy <100283390+adexp01@users.noreply.github.com>
2024-02-15 10:22:33 +0200

    fix: git ignore, config

---
+++ out/_next/static/NsrpJWKFvo0rMtnOGPXbS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/241-5dd75e0a06af8c06.js","static/chunks/pages/index-6f53b18078a259c7.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
