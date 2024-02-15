76cede8e02174f0e36410173e9a7ab5ee109f6bd
Andriy <100283390+adexp01@users.noreply.github.com>
2024-02-15 10:58:23 +0200

    chore: remove request to api

--- src/components/layout/TweetPost/TweetPost.tsx
+++ src/components/layout/TweetPost/TweetPost.tsx
@@ -3,36 +3,36 @@ import { useContext, useEffect, useState } from "react";
 import { FormDataContext } from "../../../context/FormDataContext";
 import axios from "axios";
 
-interface IEmbedTweetRes {
-  url: string;
-  author_name: string;
-  author_url: string;
-  html: string;
-  width: number;
-  height: number | null;
-  type: string;
-  cache_age: string;
-  provider_name: string;
-  provider_url: string;
-  version: string;
-}
+// interface IEmbedTweetRes {
+//   url: string;
+//   author_name: string;
+//   author_url: string;
+//   html: string;
+//   width: number;
+//   height: number | null;
+//   type: string;
+//   cache_age: string;
+//   provider_name: string;
+//   provider_url: string;
+//   version: string;
+// }
 
 const TweetPost = () => {
   const { postData } = useContext(FormDataContext);
-  const [postDescData, setPostDescData] = useState<IEmbedTweetRes | null>(null);
+  // const [postDescData, setPostDescData] = useState<IEmbedTweetRes | null>(null);
 
-  const getTweetData = async () => {
-    const res = await axios.post(`/api/twitter`, {
-      url: postData?.postUrl,
-    });
-    setPostDescData(res.data);
-  };
+  // const getTweetData = async () => {
+  //   const res = await axios.post(`/api/twitter`, {
+  //     url: postData?.postUrl,
+  //   });
+  //   setPostDescData(res.data);
+  // };
 
-  useEffect(() => {
-    if (postData?.postUrl) {
-      getTweetData();
-    }
-  }, [postData?.postUrl]);
+  // useEffect(() => {
+  //   if (postData?.postUrl) {
+  //     getTweetData();
+  //   }
+  // }, [postData?.postUrl]);
 
   function extractTweetIdFromUrl(url: string | undefined) {
     const regex = /status\/(\d+)/;
@@ -40,7 +40,7 @@ const TweetPost = () => {
     return match ? match[1] : "No result";
   }
 
-  console.log(postDescData?.html);
+  // console.log(postDescData?.html);
 
   return (
     <div>
